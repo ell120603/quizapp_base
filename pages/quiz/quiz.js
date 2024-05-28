edcd3bd60caeb51f44c704e209e582ff644454f3
eliel <elielcamara2@gmail.com>
2024-05-27 21:07:12 -0300

    trocar tema html

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,4 +1,4 @@
-import { trocarTema,verificarTema } from "./helpers/tema-helper"
+import { trocarTema,verificarTema} from "../../helpers/tema-helper.js"
 const buttonTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 buttonTema.addEventListener("click",()=>{
