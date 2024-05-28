764501a57e2023da7aea298e8eb5565c49f3f584
eliel <elielcamara2@gmail.com>
2024-05-27 22:09:04 -0300

    adicao funcao de escolher tipo quiz

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-import { trocarTema,verificarTema } from "./helpers/tema-helper"
+import { trocarTema,verificarTema } from "./helpers/tema-helper.js"
 const buttonTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 buttonTema.addEventListener("click",()=>{
     trocarTema(body,buttonTema)
 })
 verificarTema(body,buttonTema)
+
+const botoesassunto = document.querySelectorAll(".assuntos button")
+botoesassunto.forEach(botao =>{
+    botao.addEventListener("click", selecionarAssunto)
+}
+)
+function selecionarAssunto(evento){
+    const assunto = evento.target.innerText
+    localStorage.setItem("assunto",assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
