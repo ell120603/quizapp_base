c5f24b655cb2bf0d4f34ddb0077bfdae2495709b
eliel <elielcamara2@gmail.com>
2024-05-27 22:41:44 -0300

    alteracao dinamica da pag

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -5,3 +5,19 @@ buttonTema.addEventListener("click",()=>{
     trocarTema(body,buttonTema)
 })
 verificarTema(body,buttonTema)
+
+
+
+const assunto = localStorage.getItem("assunto")
+function alterarAssunto(){
+const divIcone = document.querySelector(".assunto_icone")
+const iconeImg = document.querySelector(".assunto_icone img")
+const titulo = document.querySelector(".assunto h1")
+
+divIcone.classList.add(assunto.toLowerCase())
+iconeImg.setAttribute("src",`../../assets/img/icon-${assunto.toLowerCase()}.svg`)
+iconeImg.setAttribute("alt",`icone de ${assunto}`)
+titulo.innerText = assunto
+}
+
+alterarAssunto()
