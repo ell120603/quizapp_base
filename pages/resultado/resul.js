f18e4ee9bc0ef77cbb14fdf90aa06fb81d6bc2b2
eliel <elielcamara2@gmail.com>
2024-05-27 21:10:26 -0300

    fim javascript tema

---
+++ pages/resultado/resul.js
@@ -0,0 +1,8 @@
+import { trocarTema,verificarTema} from "../../helpers/tema-helper.js"
+const buttonTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+buttonTema.addEventListener("click",()=>{
+    trocarTema(body,buttonTema)
+})
+verificarTema(body,buttonTema)
+///tema
