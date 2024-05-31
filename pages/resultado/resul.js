22752e1ff91ee82670c441406efbcf579fe550b9
eliel <elielcamara2@gmail.com>
2024-05-30 21:14:52 -0300

    adicionado a funcao de exibir resultado

--- pages/resultado/resul.js
+++ pages/resultado/resul.js
@@ -1,8 +1,34 @@
 import { trocarTema,verificarTema} from "../../helpers/tema-helper.js"
 const buttonTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
+const assunto = localStorage.getItem("assunto");
 buttonTema.addEventListener("click",()=>{
     trocarTema(body,buttonTema)
 })
 verificarTema(body,buttonTema)
 ///tema
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone");
+    const iconeImg = document.querySelector(".assunto_icone img");
+    const titulo = document.querySelector(".assunto h1");
+  
+    divIcone.classList.add(assunto.toLowerCase());
+    iconeImg.setAttribute(
+      "src",
+      `../../assets/img/icon-${assunto.toLowerCase()}.svg`
+    );
+    iconeImg.setAttribute("alt", `icone de ${assunto}`);
+    titulo.innerText = assunto;
+  }
+  alterarAssunto()
+  function inserirResultado(){
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+    sectionPontuacao.innerHTML = `
+    ${divAssunto.outerHTML}
+    <strong>${pontos}</strong>
+    <p>de 10</p>
+    `
+  }
+  inserirResultado()
